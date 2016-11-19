78d8ec285a459fda3ea1edf57a1edb8068e00a24
primary157 <victorgvbh@gmail.com>
2016-11-19 00:53:47 -0200

    Recursive QuickSort Created but not documented nor tested

--- src/quicksorts/recquick.c
+++ src/quicksorts/recquick.c
@@ -1,2 +1,48 @@
 #include "recquick.h"
+void particao(TLista *lista, int *i, int *j){
+	TItem pivo, aux;
+	*i = lista->esq;
+	*j = lista->dir;
+	pivo = lista->itens[(*i+*j)/2];
+	do {
+		while (pivo.chave > lista->itens[*i].chave) {
+			(*i)++;
+		}
+		while (pivo.chave < lista->itens[*j].chave) {
+			(*j)--;
+		}
+		if (*i <= *j) {
+			aux = lista->itens[*i];
+			lista->itens[*i] = lista->itens[*j];
+			lista->itens[*j] = aux;
+			(*i)++;
+			(*j)--;
+		}
+	} while (*i <= *j);
 
+}
+void _ordena(int esq, int dir, TLista *lista){
+	int i,j;
+	particao(lista,&i,&j);
+	if(esq < j){
+		_ordena(esq, j, lista);
+	}
+	if(i < dir){
+		_ordena(i,dir,lista);
+	}
+}
+void ordena(TLista *lista){
+	int i,j;
+	particao(lista,&i,&j);
+	if(lista->esq < j){
+		_ordena(lista->esq, j, lista);
+	}
+	if(i < lista->dir){
+		_ordena(i,lista->dir,lista);
+	}
+}
+void initTLista(TLista *lista,TItem *itens,int n){
+	lista->itens = itens;
+	lista->esq = 0;
+	lista->dir = n-1;
+}
--- src/quicksorts/recquick.h
+++ src/quicksorts/recquick.h
@@ -1,4 +1,14 @@
 #ifndef RECQUICK_H_INCLUDED
 #define RECQUICK_H_INCLUDED
-
+typedef struct{
+	int chave;
+} TItem;
+typedef struct{
+	int esq, dir;
+	TItem *itens;
+} TLista;
+void initTLista(TLista *lista,TItem *itens,int n);
+void particao(TLista *lista, int *i, int *j); //iteradores i e j são criados dentro de ordena!!
+void ordena(TLista *lista);
+void _ordena(int esq, int dir, TLista *lista);
 #endif
