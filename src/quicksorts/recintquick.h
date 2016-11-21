c7fdb7105ef7f2d5f607644e13704a0b09139d3a
primary157 <victorgvbh@gmail.com>
2016-11-20 23:19:47 -0200

    Completed?

--- src/quicksorts/recintquick.h
+++ src/quicksorts/recintquick.h
@@ -1,4 +1,9 @@
 #ifndef RECINTQUICK_H_INCLUDED
 #define RECINTQUICK_H_INCLUDED
+#include "lista.h"
+#include "recquick.h"
+void rec_int_ordena(TLista *lista);	//Função ordena q chama med_partição
+
+
 
 #endif
