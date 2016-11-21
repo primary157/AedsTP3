3b9df52a62e29bf48ff25da2fc9eaae59d1c9369
Mylyto <Adriano_mqsmts@outlook.com.br>
2016-11-20 22:14:13 -0200

    helpe-me

--- src/quicksorts/insquick.h
+++ src/quicksorts/insquick.h
@@ -1,4 +1,7 @@
 #ifndef INSQUICK_H_INCLUDED
 #define INSQUICK_H_INCLUDED
+#include "lista.h"
 
+void particao(TLista *lista, int *i, int *j); //iteradores i e j são criados dentro de ordena!!
+void ordena(TLista *lista);
 #endif
