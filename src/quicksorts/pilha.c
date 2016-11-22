2e195add9cf83d29e03df7ea673dd6d9b3aeda3d
Mylyto <Adriano_mqsmts@outlook.com.br>
2016-11-21 22:38:45 -0200

    Talvez necessário decida o que fazer

---
+++ src/quicksorts/pilha.c
@@ -0,0 +1,23 @@
+#include "pilha.h"
+
+void iniPilha(TPilha* p, int tam){
+    p->top = 0;
+    p->tam = tam;
+    p->vecI = (int*)malloc((tam*2)*sizeof(int));
+}
+int empilha(TPilha* p, int a){
+    if(p->top == p->tam)
+        return 0;
+    p->vecI[p->top++] = a;
+    return 1;
+}
+int desempilha(TPilha* p){
+    if(p->top == 0)
+        return 0;
+    return p->vecI[--p->top];
+}
+
+int destroyTPilha(TPilha* p){
+    free(p->vecI);
+    return 1;
+}
