074135f739082873f69ec6607002575e9a74c43c
Mylyto <Adriano_mqsmts@outlook.com.br>
2016-11-21 22:52:05 -0200

    Atualizado. testar se está funcional

--- src/quicksorts/iteintquick.c
+++ src/quicksorts/iteintquick.c
@@ -1,1 +1,58 @@
 #include "iteintquick.h"
+
+void ordena(TLista *lista){
+        int i, j;
+	TPilha p;
+	iniPilha(&p, lista->dir);
+
+    particao(lista, &i,&j);
+    if((j-lista->esq) > (lista->dir-i)){//Verifica se a partição da esquerda é a maior, ou seja é empilhada primeiro
+        empilha(&p, lista->esq);
+        empilha(&p, j);
+        empilha(&p, i);
+        empilha(&p, lista->dir);
+    }else{// se não a da direira é empilhada primeiro
+        empilha(&p, i);
+        empilha(&p, lista->dir);
+        empilha(&p, lista->esq);
+        empilha(&p, j);
+    }
+
+    do{ // repte o processo enquanto houverem elementos a serem desempilhados
+        lista->dir = desempilha(&p);
+        lista->esq = desempilha(&p);
+        particao(lista,&i,&j);
+        if((j-lista->esq) > (lista->dir-i)){
+            if(i<lista->dir && j>lista->esq){
+                empilha(&p, lista->esq);
+                empilha(&p, j);
+                empilha(&p, i);
+                empilha(&p, lista->dir);
+            }else if(i<lista->dir){
+                empilha(&p, i);
+                empilha(&p, lista->dir);
+            }else if(j>lista->esq){
+                empilha(&p, lista->esq);
+                empilha(&p, j);
+            }
+        }else{
+            if(i<lista->dir && j>lista->esq){
+                empilha(&p, i);
+                empilha(&p, lista->dir);
+                empilha(&p, lista->esq);
+                empilha(&p, j);
+            }else if(i<lista->dir){
+                empilha(&p, i);
+                empilha(&p, lista->dir);
+            }else if(j>lista->esq){
+                empilha(&p, lista->esq);
+                empilha(&p, j);
+            }
+        }
+
+    }while(p.top > 0);
+    destroyTPilha(&p);// desaloca o vetor de indeces da pilha.
+
+
+}
+
